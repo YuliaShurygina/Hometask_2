d3d6a15460a8456f0d9a6471916bcfbed930fce3
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-23 12:45:43 +0300

    If a number is a square of another number

---
+++ 2Square/Program.cs
@@ -0,0 +1,18 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+Console.Write("Введите число №1  ");
+int numberA = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите число  №2  ");
+int numberB = int.Parse(Console.ReadLine()??"0");
+
+if (numberA /numberB  == numberB)
+{
+    Console.WriteLine("Да, первое число является квадратом второго");
+}
+else if (numberB /numberA  == numberA)
+{
+    Console.WriteLine("Да, второе число является квадратом первого");
+}
+else
+{
+    Console.WriteLine("Нет, ни одно из чисел не является квадратом другого");
+}
