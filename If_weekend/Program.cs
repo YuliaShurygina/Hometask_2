a9a3208dd185bbaf651a22152b6f097bb82d92eb
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-23 12:34:25 +0300

    If a day is a weekend

---
+++ If_weekend/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите порядковый номер дня недели от 1(понедельник) до 7(воскресенье): ");
+int number = int.Parse(Console.ReadLine()??"0");
+if (number <= 5)
+{
+    Console.WriteLine("Будний день");
+}
+else
+{
+    Console.WriteLine("является выходным днем");
+}
+
+
