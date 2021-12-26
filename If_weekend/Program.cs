b2e3c7dca6c8f73bb3297dea93a50beddb7e2ba4
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-26 23:15:44 +0300

    hello world deleted

--- If_weekend/Program.cs
+++ If_weekend/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
-Console.WriteLine("Hello, World!");
+
 Console.WriteLine("Введите порядковый номер дня недели от 1(понедельник) до 7(воскресенье): ");
 int number = int.Parse(Console.ReadLine()??"0");
 if (number <= 5)
