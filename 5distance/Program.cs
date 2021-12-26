fdc72b20ec1f6a4b448230e5b6bc3b4b624771e4
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-26 23:11:22 +0300

    distance between two points

---
+++ 5distance/Program.cs
@@ -0,0 +1,12 @@
+﻿//Найти расстояние между точками в пространстве 2D
+Console.WriteLine("Введите координату X точки A : ");
+double xa = double.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите координату Y точки A : ");
+double ya = double.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите координату X точки B : ");
+double xb = double.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите координату Y точки B : ");
+double yb = double.Parse(Console.ReadLine()??"0");
+
+double distance  = Math.Sqrt(((xb - xa)*(xb-xa)) + ((yb - ya)*(yb-ya)));
+Console.WriteLine("Расстояние между точками A и B = " + distance);
