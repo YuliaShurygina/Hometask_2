2646b131e34462e32ec03b21a6dfeb02a0692538
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-26 22:38:19 +0300

    Palindrom

---
+++ 4Paliindrom/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа проверяет пятизначное число на палиндромом
+//12345 - порядок цифр
+//25352
+Console.WriteLine("Введите пятизначное число : ");
+int number = int.Parse(Console.ReadLine()??"0");
+int first = number / 10000;
+int second = number % 10000 / 1000;
+int fourth = number % 100 / 10;
+int fifth = number % 10; 
+if  ((first == fifth) & (second == fourth))
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число НЕ является палиндромом");
+}
