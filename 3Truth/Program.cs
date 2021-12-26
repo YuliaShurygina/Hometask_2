a3a77e5a4a1b5c39f453a45d6bd7347addd5d080
Yulia Shurygina <zavalishina-yu@mail.ru>
2021-12-26 22:11:38 +0300

    True or false

---
+++ 3Truth/Program.cs
@@ -0,0 +1,22 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool X = true;
+bool Y = false;
+
+
+
+//Console.Write("Введите значение Х: ");
+//string x = Console.ReadLine();
+//bool X = Boolean.Parse(x);
+
+//Console.Write("Введите значение Y: ");
+//string y = Console.ReadLine();
+//bool Y = Boolean.Parse(y);
+
+if ((!(X | Y)) == (!X & !Y))
+    Console.WriteLine("Высказывание истинно");
+else
+    Console.WriteLine("Высказывание ложно");
+
+
+
